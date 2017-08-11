50af8a685d12ae878779f8bdc4733dda58704f69
Andy Spohn <andy@cfn-stacks.com>
2017-08-11 07:52:57 -0400

    publish docs to site

--- publish-docs.sh
+++ publish-docs.sh
@@ -1,1 +1,20 @@
 #!/usr/bin/env bash
+
+set -ex
+
+pip install --user awscli; export PATH=$PATH:$HOME/.local/bin
+
+PROJECT_VERSION=$(./gradlew projectVersion)
+
+# Publish the site for the current version (always)
+publish_site $PROJECT_VERSION
+
+# If this is a release (not a snapshot) then publish as latest as well
+if [[ $PROJECT_VERSION != *"SNAPSHOT"* ]]; then
+    publish_site "latest"
+fi
+
+publish_site() {
+    aws s3 sync build/asciidoc/html5/ "s3://cfn-stacks.com/docs/artifacts3-repo/${1}"
+    aws s3 cp build/asciidoc/pdf/index.pdf "s3://cfn-stacks.com/docs/artifacts3-repo/${1}/artifacts3-repo.pdf"
+}
