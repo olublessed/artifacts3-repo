03d07fa214bb3daf82bf2a56905d5d0e262fbae5
Andy Spohn <andy@cfn-stacks.com>
2017-08-11 08:34:58 -0400

    remove cache

--- publish-docs.sh
+++ publish-docs.sh
@@ -4,7 +4,7 @@ set -ex
 
 pip install --user awscli; export PATH=$PATH:$HOME/.local/bin
 
-PROJECT_VERSION=$(./gradlew --no-daemon --no-build-cache --quiet projectVersion)
+PROJECT_VERSION=$(./gradlew --quiet projectVersion)
 
 publish_site() {
     aws s3 sync build/asciidoc/html5/ "s3://cfn-stacks.com/docs/artifacts3-repo/${1}"
